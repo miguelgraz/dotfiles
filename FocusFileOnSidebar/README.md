977f73d30f6d00884db427c51f6dd34268a87f73
Miguel Grazziotin <miguelgraz@gmail.com>
2014-06-27 16:48:16 -0300

    remove plugins code and add a small script to clone them all right from their repos

--- FocusFileOnSidebar/README.md
+++
@@ -1,4 +0,0 @@
-FocusFileOnSidebar
-==================
-
-Sublime Text plugin to open sidebar and focus on the current opened file
---
+++ install_ST_plugins.sh
@@ -0,0 +1,6 @@
+cd ~/.config/sublime-text-3/Packages/
+
+git clone git://github.com/matthewrobertson/ERB-Sublime-Snippets.git ERB_Snippets
+git clone git@github.com:miguelgraz/FocusFileOnSidebar.git
+git clone git@github.com:FMCorz/MoveTab.git
+git clone git@github.com:miguelgraz/ST2-GitHubinator.git
