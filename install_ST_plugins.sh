ca56d54e42812427d8a121a56bdfce0c2c7265e1
Miguel Grazziotin <miguelgraz@gmail.com>
2014-07-23 23:00:46 -0300

    Use the original GitHubinator now that I got my PR merged :D

--- install_ST_plugins.sh
+++ install_ST_plugins.sh
@@ -7,4 +7,4 @@ cd ~/.config/sublime-text-3/Packages/
 git clone git://github.com/matthewrobertson/ERB-Sublime-Snippets.git ERB_Snippets
 git clone git@github.com:miguelgraz/FocusFileOnSidebar.git
 git clone git@github.com:FMCorz/MoveTab.git
-git clone git@github.com:miguelgraz/ST2-GitHubinator.git
+git clone git@github.com:ehamiter/ST2-GitHubinator.git
