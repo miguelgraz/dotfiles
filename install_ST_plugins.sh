d9d9348cc79057132a48d01bb64f30ca7db3970b
Miguel Grazziotin <miguelgraz@gmail.com>
2016-01-19 23:04:53 -0200

    Install and use Material Theme, improve user preferences with more regular Sublime settings and some MaterialTheme-specific settings

--- install_ST_plugins.sh
+++ install_ST_plugins.sh
@@ -1,10 +1,11 @@
 cd ~/Desktop ; git clone git@github.com:miguelgraz/dotfiles.git ;
-cp dotfiles/TwilightBold.tmTheme ~/.config/sublime-text-3/Cache/Color\ Scheme\ -\ Default/
+cp dotfiles/TwilightBold.tmTheme ~/.config/sublime-text-3/Packages
 cp -r dotfiles/User ~/.config/sublime-text-3/Packages/
 rm -rf dotfiles ;
 
 cd ~/.config/sublime-text-3/Packages/
 
+git clone git@github.com:equinusocio/material-theme.git Material\ Theme
 git clone git://github.com/matthewrobertson/ERB-Sublime-Snippets.git ERB_Snippets
 git clone git@github.com:miguelgraz/FocusFileOnSidebar.git
 git clone git@github.com:FMCorz/MoveTab.git
