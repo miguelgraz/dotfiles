c8e92fd0c5fd1bfd3b4515f3b3ebb90646b2ea5f
Miguel Grazziotin <miguelgraz@gmail.com>
2016-04-23 22:05:49 -0300

    Notify using the song title as summary and the artist as body

--- notify_script.sh
+++ notify_script.sh
@@ -3,7 +3,7 @@
 # Script to send a notification about the current playing song
 # Called from MOC's option "OnSongChange" and custom keyboard shortcut
 
-/usr/bin/notify-send "`mocp --format %artist` - `mocp --format %song`";
+/usr/bin/notify-send "`mocp --format %song`" "`mocp --format %artist`";
 # notify-send ignores the timeout parameter
 # so we kill the process manually
 sleep 1;
