162c99d0a360f1b531607d26744b72f802df6fe7
Miguel Schmitz Grazziotin <miguelgraz@gmail.com>
2018-06-06 17:26:49 +0200

    Update notify_script to work with spotify instead of MOC

--- notify_script.sh
+++ notify_script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Script to send a notification about the current playing song
-# Called from MOC's option "OnSongChange" and custom keyboard shortcut
+# Called from "spotify-now" available on https://github.com/getmicah/spotify-now and custom keyboard shortcut
+# Notification built using the same format as Spotify's default
 
-/usr/bin/notify-send "`mocp --format %song`" "`mocp --format %artist`";
+/usr/bin/notify-send -i audio-x-generic "`spotify-now -i '%title'`" "`spotify-now -i '%artist - %album'`";
 # notify-send ignores the timeout parameter
 # so we kill the process manually
 sleep 1;
