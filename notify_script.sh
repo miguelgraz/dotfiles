05fa1b7622d00eef46addfd27c0e5ce8df1ba87e
Miguel Schmitz Grazziotin <miguelgraz@gmail.com>
2018-06-07 10:58:44 +0200

    Add param to notify-send to remove the notification from tray automatically

--- notify_script.sh
+++ notify_script.sh
@@ -4,7 +4,7 @@
 # Called from "spotify-now" available on https://github.com/getmicah/spotify-now and custom keyboard shortcut
 # Notification built using the same format as Spotify's default
 
-/usr/bin/notify-send -i audio-x-generic "`spotify-now -i '%title'`" "`spotify-now -i '%artist - %album'`";
+/usr/bin/notify-send --hint=int:transient:1 -i audio-x-generic "`spotify-now -i '%title'`" "`spotify-now -i '%artist - %album'`";
 # notify-send ignores the timeout parameter
 # so we kill the process manually
 sleep 1;
